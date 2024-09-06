6d032e50b9c976cf4f7cbbbc8b339abb50b05664
Krish Bharadwaj <167067349+KrishBharadwaj5678@users.noreply.github.com>
2024-09-06 10:22:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import speedtest
+import subprocess
+
+st.set_page_config(
+    page_title="Speedify",
+    page_icon="icon.png",
+    menu_items={
+        "About":"Speedify offers a quick and accurate way to measure your internet speed. Test your download, upload, and ping speeds with ease!"
+    }
+)
+
+st.write("<h2 style='color:#6CB3FF;'>Test Your Internet Speed Instantly</h2>",unsafe_allow_html=True)
+
+result=subprocess.run(["speedtest-cli","--share"],capture_output=True,text=True)
+output=result.stdout
+link=output.split("results:")[1].strip()
+btn=st.button("Test Speed")
+if btn:
+    st.image(link)
